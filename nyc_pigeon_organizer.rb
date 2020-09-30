7752f7d3fa73ff0f55063b5d3772262fecc7de53
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-30 21:41:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,10 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(keys, values), nyc_pigeon|
  values.each do |in_keys, names|
    names.each do |name|
-     if nyc_pigeon[name]
+     if !nyc_pigeon[name]
        nyc_pigeon[name] = {}
      end
-     if 
+     if  
   binding.pry
   nyc_pigeon
     end    
