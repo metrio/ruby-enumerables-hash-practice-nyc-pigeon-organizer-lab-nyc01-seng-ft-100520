04dc3571e23e67865d02f5109a43ed42438fbf1b
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-30 22:10:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ 
+  data.each_with_object({}) do |(keys, values), nyc_pigeon|
+ values.each do |in_keys, names|
+   names.each do |name|
+     if !nyc_pigeon[name]
+       nyc_pigeon[name] = {}
+     end
+     if !nyc_pigeon[name][keys]
+       !nyc_pigeon[name][keys] = []
+     end
+  nyc_pigeon[name][keys].push(in_keys.to_s)
+      end    
+    end
+  end
 end
