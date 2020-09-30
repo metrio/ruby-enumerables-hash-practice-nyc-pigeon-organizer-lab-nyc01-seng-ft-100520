ce61c58c065c97236aa79dc383a7e3666da3490f
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-30 21:45:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,8 +10,8 @@ def nyc_pigeon_organizer(data)
      if !nyc_pigeon[names][keys]
        !nyc_pigeon[names][keys] = {}
        
-
+end
   nyc_pigeon[name][keys].push(in_keys)
     end    
 
-  end
+end
