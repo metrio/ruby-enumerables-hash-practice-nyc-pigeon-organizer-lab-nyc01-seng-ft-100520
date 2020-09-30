3f6e3eb0fbed3f9af4edeab8a8302898fbfc6294
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-30 21:13:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ 
+  data.each_with_object({}) do |(keys, values), nyc_pigeon|
+ values.each do |in_keys, names|
+   names.each do |name|
+     
+  binding.pry
+  nyc_pigeon
+      end
+    end    
+binding.pry
+  end
 end
